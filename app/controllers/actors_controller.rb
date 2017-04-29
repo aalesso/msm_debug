ac6a2d52a13187c27055c68b7d4192335b1ff12b
aalesso <aalesso@users.noreply.github.com>
2017-04-29 18:08:12 -0500

    last3

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -24,19 +24,34 @@ class ActorsController < ApplicationController
 
   def edit_form
     @actor = Actor.find(params[:id])
+    # @actor.dob = params[:dob]
+    # @actor.name = params[:name]
+    # @actor.bio = params[:bio]
+    # @actor.image_url = params[:image_url]
+    render("actors/edit_form.html.erb")
   end
 
   def update_row
-    @actor = Actor.find(params[:id])
-
+    the_id = params[:id]
+    @actor = Actor.find(the_id)
     @actor.dob = params[:dob]
     @actor.name = params[:name]
     @actor.bio = params[:bio]
     @actor.image_url = params[:image_url]
-
+#
+#     dob2 = params[:dob]
+#     name2 = params[:name]
+#     bio2 = params[:bio]
+#     image_url2 = params[:image_url]
+# updated_actor=Actor.find(the_id)
+# updated_actor.dob=dob2
+# updated_actor.name=name2
+# updated_actor.bio=bio2
+# updated_actor.image_url=image_url2
+# updated_actor.save
     @actor.save
 
-    render("show")
+    redirect_to("/actors")
   end
 
   def destroy
